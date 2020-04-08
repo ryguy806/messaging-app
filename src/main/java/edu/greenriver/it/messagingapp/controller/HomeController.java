1ddaa4ae655a89cefafac1d98973e3b31025c770
RyGuy8806 <L1ghtSab3r11!!>
2020-04-08 11:33:57 -0700

    Added repo to controller

--- src/main/java/edu/greenriver/it/messagingapp/controller/HomeController.java
+++ src/main/java/edu/greenriver/it/messagingapp/controller/HomeController.java
@@ -1,14 +1,25 @@
 package edu.greenriver.it.messagingapp.controller;
 
 //The framework imports for using the @ links.
+import edu.greenriver.it.messagingapp.repositories.IMessagesRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping
-    public String home(){
+    private IMessagesRepository repo;
+
+    public HomeController(IMessagesRepository repo){
+        this.repo = repo;
+    }
+
+    @RequestMapping("/")
+    public String home(Model model){
+
+        //Saves all messages
+        model.addAttribute("messages", repo.findAll());
 
         //The job of this method is to return the name of the template.
         return "index";
